80831c984ae9fabb8b44a373ba05afaaed09f3e7
charanyaxp <charanya.xvalue@gmail.com>
2025-07-03 12:47:10 +0530

    Added agents

---
+++ notification_agent.py
@@ -0,0 +1,15 @@
+from langgraph.graph import StateGraph
+from agents.utils import get_connection_string, get_llm, get_sql_database_toolkit
+from langgraph.prebuilt import create_react_agent
+from tools.email import individual_email_tool
+
+
+connection_string = get_connection_string()
+llm = get_llm()
+sql_toolkit = get_sql_database_toolkit(llm=llm)
+email_tool = individual_email_tool
+
+agent = create_react_agent(
+    model=llm,
+    tools= sql_toolkit.get_tools() + [email_tool]
+)
