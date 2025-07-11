74ecc3771e823eb94c953b2d0c11e6df663ba129
charanyaxp <charanya.xvalue@gmail.com>
2025-07-11 13:21:46 +0530

    code cleanup

--- MCP/ECommerce/utils.py
+++ MCP/ECommerce/utils.py
@@ -4,10 +4,8 @@ from dotenv import load_dotenv
 import os
 import mysql.connector
 from typing import Union
-#from mysql.connector.pooling import PooledMySQLConnection, MySQLConnectionAbstract
 
-
-def get_database_connection(): # -> Union[PooledMySQLConnection, MySQLConnectionAbstract]:
+def get_database_connection(): 
     """This method connects to the mysql database
 
     Returns:
@@ -21,23 +19,3 @@ def get_database_connection(): # -> Union[PooledMySQLConnection, MySQLConnection
         database=os.getenv('MYSQL_DATABASE')
     )
     return connection
-
-
-if __name__ == '__main__':
-    connection = get_database_connection()
-    cursor = connection.cursor()
-    # name = 'Sony PlayStation5 Gaming Console'
-    # description = 'Slim Design: With PS5, players get powerful gaming technology packed inside a sleek and compact console design. 1TB of Storage: Keep your favorite games ready and waiting for you to jump in and play with 1TB of SSD storage built in. Ultra-High Speed SSD: Maximize your play sessions with near instant load times for installed PS5 games.'
-    # price = 54990
-    # stock_quantity = 10
-    # category_id = 1
-    # image_url = "https://m.media-amazon.com/images/I/51Ex3GcYMIL._SL1000_.jpg"
-
-    # insert_product_sql = f"INSERT INTO products (name, description, price, stock_quantity, category_id, image_url) VALUES ('{name}', '{description}', {price}, {stock_quantity}, {category_id}, '{image_url}')"
-    # cursor.execute(insert_product_sql)
-    # connection.commit()
-    latest_product_sql = 'SELECT * FROM `products` ORDER BY product_id DESC LIMIT 1;'
-    cursor.execute(latest_product_sql)
-    products = cursor.fetchall()
-    for product in products:
-        print(product)
